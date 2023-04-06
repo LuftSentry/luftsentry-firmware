99e11556231581940a362014f56067a36e3e8b1c
Andrés Vásquez <admonit.ingenieria@udea.edu.co>
2023-04-06 10:12:28 -0500

    feat: Implemented MQTT function in the project

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,25 @@
 // Library
 #include <WiFi.h>
-
+#include <SPIFFS.h>
+#include <PubSubClient.h>
+#include "MQTT.hpp"
 
 // Utils
 #include "config.h" 
 #include "ESP32_Utils_WiFi.hpp"
+#include "ESP32_Utils_MQTT.hpp"
 
 void setup(void)
 {
 	Serial.begin(115200);
-
+	SPIFFS.begin();
 	ConnectWiFi_STA(false);
+
+	InitMqtt();
 
 }
 void loop() {
-  // put your main code here, to run repeatedly:
+	HandleMqtt();
+
+	delay(20000);
 }
